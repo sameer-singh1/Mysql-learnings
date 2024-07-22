5928b6d1e6e5f03a10a0eecc3aa5a3e1f6674ec9
sameer-singh1 <sameersinghlnctu@gmail.com>
2024-07-22 20:56:48 +0530

    files added

--- sql6.sql
+++ sql6.sql
@@ -13,5 +13,24 @@ select s_name from s1 union select c_per from c1;
 insert into c1 values (4,'A',70);
 
 /*union operator selects only distinct values. to allow duplicate values use UNION ALL. */
-select s_name from s1 union all select c_name from c1; 
+select s_name from s1 union all select c_name from c1;
 
+insert into s1 values (104, 'D',100);
+
+select s_id from s1 where s_per <any (select s_per from s1 where s_per = 90); 
+
+/*find the name whose salary is greter than the salary of any customer whose age is 25 */
+select * from dept;
+select salary from dept where age = 32;
+select name from dept where salary>2000;
+select name from dept where salary>(select salary from dept where age = 32); 
+select name from dept where salary>any(select salary from dept where age = 25);/*age = 25 is two times*/
+/*in ANY condition atleast one condition should be true*/
+select name from dept where salary>all(select salary from dept where age = 25);/*in all both condition should be true*/
+
+/*find the name of customer whose salary is less than the avg salary of customer*/
+select name from dept where salary < (select avg(salary) from dept);
+
+select name from dept where salary>some(select salary from dept where age = 25);
+
+select * from dept where exists (select name from dept where age = 22);
