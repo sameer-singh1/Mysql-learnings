f1409d82014d929e17bd7f6a010edffbb1ecf509
sameer-singh1 <sameersinghlnctu@gmail.com>
2024-08-01 01:35:01 +0530

    files added

---
+++ sql9.sql
@@ -0,0 +1,16 @@
+/*Regular expression*/
+use sameer;
+select count(*) from employees;
+
+/*expression-'^'*/
+select email from employees where email regexp '^d';/*email starts with d*/
+select email from employees where email regexp 'ay$';/*email ends with ay*/
+select email from employees where email regexp 'ro?';/*email with r,o and ro*/
+select email from employees where email regexp 'ro+';/*email with ro*/
+select email from employees where email regexp 'ro*';/*email with r,o and ro*/
+select email from employees where email regexp 'so|ro';/*email with ro and so*/
+select email from employees where email regexp '[ba]';/*email with ba*/
+select email from employees where email regexp '[ba|do]';
+select email from employees where email regexp '[a-d].[o]';/*email with a to d then any character followed by o*/
+select email from employees where email regexp '[a-d]...[o]';/*email with a to d then any 3 character followed by o*/
+select email from employees;
